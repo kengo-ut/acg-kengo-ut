791a42c6adb4c14b495ee48643e0846bec254523
kengo-ut <keng1001uni@g.ecc.u-tokyo.ac.jp>
2024-04-25 02:43:19 +0900

    task02 finished

--- task02/main.cpp
+++ task02/main.cpp
@@ -65,8 +65,129 @@ int number_of_intersection_ray_against_quadratic_bezier(
     const Eigen::Vector2f &pc,
     const Eigen::Vector2f &pe) {
   // comment out below to do the assignment
-  return number_of_intersection_ray_against_edge(org, dir, ps, pe);
+  // return number_of_intersection_ray_against_edge(org, dir, ps, pe);
   // write some code below to find the intersection between ray and the quadratic
+  // Bézier curve defined by ps, pc, and pe.
+  const auto p = [&ps, &pc, &pe](float t)
+  {
+    return (1 - t) * (1 - t) * ps + 2 * (1 - t) * t * pc + t * t * pe;
+  };
+  const auto p_prime = [&ps, &pc, &pe](float t)
+  {
+    return 2 * (1 - t) * (pc - ps) + 2 * t * (pe - pc);
+  };
+  const auto p_double_prime = 2 * (pe - 2 * pc + ps);
+  const auto w = Eigen::Vector2f(-dir[1], dir[0]);
+  const auto f0 = [&org, &p, &w](float t)
+  {
+    return (p(t) - org).dot(w);
+  };
+  const auto f1 = [&p_prime, &w](float t)
+  {
+    return p_prime(t).dot(w);
+  };
+  const float f1_prime = p_double_prime.dot(w);
+  const float a = f1_prime / 2;
+  const float b = f1(0);
+  const float c = f0(0);
+
+  // method 1. solve the quadratic equation directly
+  if (b * b - 4 * a * c < 0) { return 0; }
+  else if (b * b - 4 * a * c == 0) { 
+    const float t = -b / (2 * a);
+    int res = 0;
+    if (0 < t && t < 1)
+    {
+      const float s = (p(t) - org)[0] / dir[0];
+      if (s > 0) {
+        res++;
+      }
+    }
+    return res;
+  }
+  else { 
+    const float t0 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+    const float t1 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+    int res = 0;
+    if (0 < t0 && t0 < 1)
+    {
+      const float s = (p(t0) - org)[0] / dir[0];
+      if (s > 0) {
+        res++;
+      }
+    }
+    if (0 < t1 && t1 < 1)
+    {
+      const float s = (p(t1) - org)[0] / dir[0];
+      if (s > 0) {
+        res++;
+      }
+    }
+    return res;
+  }
+  
+  // method 2. solve the quadratic equation using sturm's method and bisection method
+  // const float d = 2 * a;
+  // const float e = b;
+  // const float f2 = -(c - b * e / d + a * e * e / (d * d));
+  // const auto count_sign_changes = [&f0, &f1, &f2](float t)
+  // {
+  //   int count = 0;
+  //   const float f0_t = f0(t);
+  //   const float f1_t = f1(t);
+  //   const float f2_t = f2;
+  //   if ((f0_t > 0 && f1_t <= 0) || (f0_t < 0 && f1_t >= 0)) { count++; }
+  //   if ((f1_t > 0 && f2_t <= 0) || (f1_t < 0 && f2_t >= 0)) { count++; }
+  //   return count;
+  // };
+  // float t_min = 0;
+  // float t_max = 1;
+  // int num_roots = count_sign_changes(t_min) - count_sign_changes(t_max);
+  // if (num_roots == 1)
+  // {
+  //   float t_mid = (t_min + t_max) / 2;
+  //   while (t_max - t_min > 1e-6) {
+  //     t_mid = (t_min + t_max) / 2;
+  //     if (count_sign_changes(t_min) - count_sign_changes(t_mid) == 1) { t_max = t_mid; }
+  //     else if (count_sign_changes(t_mid) - count_sign_changes(t_max) == 1) { t_min = t_mid; }
+  //     else { break; }
+  //   }
+  //   const float s = (p(t_mid) - org)[0] / dir[0];
+  //   if (s > 0) {
+  //     return 1;
+  //   }
+  // }
+  // else if (num_roots == 2) {
+  //   float t_min0 = t_min;
+  //   float t_max0 = - b / (2 * a);
+  //   float t_min1 = - b / (2 * a);
+  //   float t_max1 = t_max;
+  //   float t_mid0 = (t_min0 + t_max0) / 2;
+  //   float t_mid1 = (t_min1 + t_max1) / 2;
+  //   while (t_max0 - t_min0 > 1e-6) {
+  //     t_mid0 = (t_min0 + t_max0) / 2;
+  //     if (count_sign_changes(t_min0) - count_sign_changes(t_mid0) == 1) { t_max0 = t_mid0; }
+  //     else if (count_sign_changes(t_mid0) - count_sign_changes(t_max0) == 1) { t_min0 = t_mid0; }
+  //     else { break; }
+  //   }
+  //   while (t_max1 - t_min1 > 1e-6) {
+  //     t_mid1 = (t_min1 + t_max1) / 2;
+  //     if (count_sign_changes(t_min1) - count_sign_changes(t_mid1) == 1) { t_max1 = t_mid1; }
+  //     else if (count_sign_changes(t_mid1) - count_sign_changes(t_max1) == 1) { t_min1 = t_mid1; }
+  //     else { break; }
+  //   }
+  //   int res = 0;
+  //   float s = (p(t_mid0) - org)[0] / dir[0];
+  //   if (s > 0) {
+  //     res++;
+  //   }
+  //   s = (p(t_mid1) - org)[0] / dir[0];
+  //   if (s > 0) {
+  //     res++;
+  //   }
+  //   return res;
+  // }
+  // return 0;
 }
 
 int main() {
