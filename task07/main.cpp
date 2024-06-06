f05cb3479738d58b3db27dde19e42e66b797ef3b
kengo-ut <keng1001uni@g.ecc.u-tokyo.ac.jp>
2024-06-06 09:07:11 +0900

    task07 finished

--- task07/main.cpp
+++ task07/main.cpp
@@ -367,7 +367,7 @@ int main() {
         if (hit1_object == -1){ continue; }
         float hit1_rad = spheres[hit1_object].emission;
         // compute the contribution for this pixel
-        float rad = 0.f; // replace this with some code
+        float rad = hit0_brdf * hit1_rad * hit0_refl.dot(hit0_normal) / (hit0_pdf * nsample);
         img_light[ih * img_width + iw] += rad;
       }
       // -----------------
@@ -386,7 +386,7 @@ int main() {
         if (hit1_object == -1){ continue; }
         float hit1_rad = spheres[hit1_object].emission;
         // compute the contribution for this pixel
-        float rad = 0.f; // replace this with some code
+        float rad = hit0_brdf * hit1_rad * hit0_refl.dot(hit0_normal) / (hit0_pdf * nsample);
         img_brdf[ih * img_width + iw] += rad;
       }
       // -----------------
@@ -404,7 +404,7 @@ int main() {
         float hit1_rad = spheres[hit1_object].emission;
         float hit0_pdf_brdf_sample = spheres[hit0_object].pdf(hit0_normal, cam_ray_dir, hit0_refl);
         float hit0_pdf_light_sample = pdf_light_sample(hit0_normal, hit0_pos, cam_ray_dir, hit0_refl, hit0_object);
-        float rad = 0.f; // write some code
+        float rad = (hit0_pdf_brdf_sample / (hit0_pdf_light_sample + hit0_pdf_brdf_sample)) * hit0_brdf * hit1_rad * hit0_refl.dot(hit0_normal) / (hit0_pdf_brdf_sample * num_half_sample);
         img_mis[ih * img_width + iw] += rad;
       }
       for (int isample = 0; isample < nsample / 2; ++isample) {
@@ -416,7 +416,7 @@ int main() {
         float hit1_rad = spheres[hit1_object].emission;
         float hit0_pdf_light_sample = pdf_light_sample(hit0_normal, hit0_pos, cam_ray_dir, hit0_refl, hit0_object);
         float hit0_pdf_brdf_sample = spheres[hit0_object].pdf(hit0_normal, cam_ray_dir, hit0_refl);
-        float rad = 0.f; // write some code
+        float rad = (hit0_pdf_light_sample / (hit0_pdf_light_sample + hit0_pdf_brdf_sample)) * hit0_brdf * hit1_rad * hit0_refl.dot(hit0_normal) / (hit0_pdf_light_sample * num_half_sample);
         img_mis[ih * img_width + iw] += rad;
       }
     }
